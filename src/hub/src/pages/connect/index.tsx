e4610792773d11cfa5e841bcef602da9076fff7a
Leontiy <leosungun@gmail.com>
2024-08-09 20:41:59 +0300

    add connect button style

--- src/hub/src/pages/connect/index.tsx
+++ src/hub/src/pages/connect/index.tsx
@@ -1,16 +1,40 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Stack, Typography, Button } from '@mui/material';
-import { Layout, LayoutBackground } from 'widgets';
-import { routes, GradientButtonWraper } from 'shared';
+import { Layout } from 'widgets';
+import { routes, GradientButtonWraper, BORDER_RADIUS_S, COLOR_BLACK } from 'shared';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@ic-reactor/react';
+
+
+export const ConnectButton = () => {
+  const { login } = useAuth();
+
+  return (
+    <GradientButtonWraper sx={{ borderRadius: BORDER_RADIUS_S, '&:hover': {boxShadow: '0px 0px 40px -15px #635D95'} }}>
+      <Button sx={{
+        backgroundColor: 'white',
+        color: COLOR_BLACK,
+        borderRadius: BORDER_RADIUS_S,
+        fontSize: '20px',
+        padding: '10px 30px',
+        '&:hover': {
+          backgroundColor: 'white',
+        }
+      }} onClick={() => {
+            login({
+              identityProvider: 'http://c2lt4-zmaaa-aaaaa-qaaiq-cai.localhost:4943/#authorize'
+            })
+      }}>Connect</Button>
+    </GradientButtonWraper>
+  )
+}
 
 export const ConnectPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { login, authenticated } = useAuth();
+  const { authenticated } = useAuth();
 
   useEffect(() => {
     if (authenticated) {
@@ -19,18 +43,12 @@ export const ConnectPage = () => {
   }, [authenticated, navigate]);
 
   return (
-    <Layout bg={LayoutBackground.Sm}>
+    <Layout>
       <Typography variant="h1" className="center" mt={{ xs: 2, md: 15, lg: 36 }} mb={3.75}>
         Connect your wallet
       </Typography>
       <Stack direction={{ xs: 'column', md: 'row' }} spacing={1} justifyContent="center" alignItems="center">
-        <GradientButtonWraper>
-          <Button onClick={() => {
-            login({
-              identityProvider: 'http://c2lt4-zmaaa-aaaaa-qaaiq-cai.localhost:4943/#authorize'
-            })
-          }}>Connect</Button>
-        </GradientButtonWraper>
+        <ConnectButton />
       </Stack>
       <Typography className="center" mt={7.5}>
         Sign in by selecting your preffered wallet
--- src/hub/src/shared/ui/gradient-button-wrapper/index.ts
+++ src/hub/src/shared/ui/gradient-button-wrapper/index.ts
@@ -2,10 +2,10 @@ import { BORDER_RADIUS_S, COLOR_PURPLE_GRADIENT, BORDER_PADDING, COLOR_BORDER_GR
 import { styled, Stack } from '@mui/material';
 
 export const GradientButtonWraper = styled(Stack)(({ theme }) => ({
-    background: COLOR_BORDER_GRAY,
+    background: COLOR_PURPLE_GRADIENT,
     position: "relative",
     padding: BORDER_PADDING,
     '&:hover': {
-      background: COLOR_PURPLE_GRADIENT
+      background: COLOR_BORDER_GRAY
     }
 }))
