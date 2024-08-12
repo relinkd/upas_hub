da0b197356e1c6698a33fedee38c2d28b4177e47
Leontiy <leosungun@gmail.com>
2024-08-12 23:41:00 +0300

    postMessage test

--- src/hub/src/app/index.tsx
+++ src/hub/src/app/index.tsx
@@ -3,11 +3,33 @@ import { ModalContainer } from 'entities/modal';
 
 import { withPersistor, withProviders, withRouter, withStore, withTheme, WithICConnect, WithAchievementProvider } from './providers';
 import { RouteManager } from './router';
+import { useEffect } from 'react';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './appStyles/index.scss';
 
 const App = () => {
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.origin !== "http://localhost:5174") {
+        return;
+      }
+
+      console.log(window.opener.location)
+
+      window.opener.postMessage('testmessage to parent', 'http://localhost:5174');
+
+      console.log('Received message:', event.data);
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+
   return (
     <>
       <RouteManager />
