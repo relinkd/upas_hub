cdf65ddcd106482e730062e937110b9b6cd9ee37
luckerninja <leosungun@gmail.com>
2024-10-12 18:27:24 +0300

    update mainnet canister addresses

--- src/hub/src/widgets/layout/ui/footer/index.tsx
+++ src/hub/src/widgets/layout/ui/footer/index.tsx
@@ -27,8 +27,8 @@ export const Footer = () => {
         borderColor: COLOR_BORDER_PURPLE,
         marginBottom: 2
     }}>
-      <FooterLink href="#">Github</FooterLink>
-      <FooterLink href="#">DOCs</FooterLink>
+      <FooterLink target="_blank" href="https://github.com/relinkd/UPAS">Github</FooterLink>
+      <FooterLink target="_blank" href="https://relinkd.gitbook.io/upas">DOCs</FooterLink>
     </Stack>
   )
 };
